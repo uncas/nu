72c9cc01412280377162701097cae9cd739e74d6
Ole Lynge Soerensen <osoerensen@ebay.com>
2018-01-07 21:53:38 +0100

    Add empty solution.

---
+++ nu/FirstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  nu
+//
+//  Created by Soerensen, Ole Lynge on 07/01/2018.
+//  Copyright (c) 2018 Uncas. All rights reserved.
+//
+
+import UIKit
+
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
---
+++ nu/SecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.swift
+//  nu
+//
+//  Created by Soerensen, Ole Lynge on 07/01/2018.
+//  Copyright (c) 2018 Uncas. All rights reserved.
+//
+
+import UIKit
+
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
